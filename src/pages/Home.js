8a80fb7121ce825cc2b3f15779d8523d9cee4fda
Saksham Gupta <sakshamprem2003@gmail.com>
2024-12-08 23:42:09 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ function Home() {
     try {
       dispatch(showLoading());
       const response = await axios.get(
-        "/api/user/get-all-approved-doctors",
+        "https://chikitsa-sewa-backend.onrender.com/api/user/get-all-approved-doctors",
 
         {
           headers: {
@@ -33,7 +33,7 @@ function Home() {
   const getUserData = async () => {
     try {
       const response = await axios.get(
-        "/api/user/get-user-info-by-id",
+        "https://chikitsa-sewa-backend.onrender.com/api/user/get-user-info-by-id",
 
         {
           headers: {
