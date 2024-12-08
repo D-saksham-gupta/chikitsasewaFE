3daa57e0d0188c0f9393bc5ed338dfa08e998105
Saksham Gupta <sakshamprem2003@gmail.com>
2024-12-08 23:41:18 +0530

    Update Notifications.js

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -17,7 +17,7 @@ function Notifications() {
     try {
       dispatch(showLoading());
       const response = await axios.post(
-        "/api/user/mark-all-notifications-as-seen",
+        "https://chikitsa-sewa-backend.onrender.com/api/user/mark-all-notifications-as-seen",
         {
           userId: user._id,
         },
@@ -43,7 +43,7 @@ function Notifications() {
     try {
       dispatch(showLoading());
       const response = await axios.post(
-        "/api/user/delete-all-notifications",
+        "https://chikitsa-sewa-backend.onrender.com/api/user/delete-all-notifications",
         {
           userId: user._id,
         },
